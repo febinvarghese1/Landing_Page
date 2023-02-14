3923ea7cadd8713ae0668e0b7c6630fca7f0543c
Febin  Varghese <febinvarghese@SSLAP0042.local>
2023-02-14 21:43:51 +0530

    changes

--- app/index.js
+++ app/index.js
@@ -10,21 +10,6 @@ const values = [start,first,second,third];
 
 
 
-// values.forEach(value=>{
-//     const rect = value.getBoundingClientRect();
-//     console.log(rect.top.toFixed())
-// })
-
-// const windowHeight = window.innerHeight;
-
-// window.addEventListener("scroll",()=>{
-//     const scrollTop = window.scrollY;
-//     values.forEach((section,i)=>{
-//         if(section.offsetTop< scrollTop + windowHeight/2 && scrollTop < section.offsetTop + windowHeight/2){
-//             console.log(i)
-//         }
-//     })
-// })
 
 
 const sliders = document.querySelectorAll(".hero__right_slider a");
@@ -43,10 +28,7 @@ const removeActiveClass = () =>{
 const addActiveClass = (entries,observer)=>{
     entries.forEach(entry=>{
         if(entry.isIntersecting){
-            let slider = document.querySelector(".hero__right_slider");
-            console.log()
             let current = document.querySelector(`.hero__right_slider a[href='#${entry.target.id}']`);
-            console.log(current);
             removeActiveClass();
             current.classList.add("active");
         }
